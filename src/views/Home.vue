29fb16f5a83ca3c4af1d7915f52f5985d6ed1c28
Makoto Uwaya <makoto.uwaya@gmail.com>
2020-02-09 09:38:07 +0900

    refs #8 不要なファイル、参照を削除

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,14 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+  <v-container>
+    <v-row>
+      <v-img alt="farms form logo" src="@/assets/logo.png" />
+    </v-row>
+  </v-container>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
 
-export default {
-  name: "Home",
-  components: {
-    HelloWorld
-  }
-};
+@Component
+export default class Home extends Vue {}
 </script>
