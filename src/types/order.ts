ea2c0e45ccc12e05dde1e0ca6d56d07a5e1165d6
Makoto Uwaya <makoto.uwaya@gmail.com>
2020-02-09 10:33:28 +0900

    refs #12 ドキュメンテーションコメントを追加

--- src/types/order.ts
+++ src/types/order.ts
@@ -1,13 +1,22 @@
 import firebase from "firebase/app";
 import "firebase/firestore";
 
+/** 注文情報 */
 export interface order {
+  /** 品目 */
   item: number;
+  /** 数量 */
   count: number;
+  /** 発送期限 */
   deadline: firebase.firestore.Timestamp;
+  /** 卸売 */
   isBusiness: boolean;
+  /** 作成日 */
   createdAt: firebase.firestore.Timestamp;
+  /** 作成者アカウント */
   creatorAccount: string;
+  /** 更新日 */
   modifiedAt: firebase.firestore.Timestamp;
+  /** 更新者アカウント */
   modifierAccount: string;
 }
