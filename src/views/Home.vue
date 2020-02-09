cf92bd179f62a34b336d7aad1b9f69f5cc53b1c8
Anizo1223 <mol9135@gmail.com>
2020-02-09 18:08:06 +0900

    refs #20 ログイン中のユーザ名(メールアドレス)を表示

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,8 +4,9 @@
       <v-img alt="farms form logo" src="@/assets/logo.png" />
     </v-row>
     <v-col align="center">
-      <div class="my-5">
-        <v-btn x-large @click="inputItemDialog = true">注文内容を入力</v-btn>
+      <div>
+        <v-text>ユーザ名: </v-text>
+        <strong>{{ name }}</strong>
       </div>
       <div class="my-5">
         <v-btn x-large width="300px" to="/archive">
@@ -55,6 +56,7 @@ import { Component, Vue } from "vue-property-decorator";
 import { getBlancOrderData, addOrder } from "@/modules/orderModule";
 import { signOut } from "@/modules/signinModule";
 import InputForm from "@/components/InputForm.vue";
+import firebase from "firebase/app";
 
 @Component({
   components: {
@@ -65,7 +67,11 @@ export default class Home extends Vue {
   inputItemDialog: boolean = false;
   successSnackbar: boolean = false;
   currentOrder = getBlancOrderData();
+  name: string = firebase.auth().currentUser.email;
 
+  async onClick() {
+    const order = getBlancOrderData();
+  }
   async onInputDialogSave() {
     await addOrder(this.currentOrder);
     console.log("注文を追加");
