e5a73a799d0ed438cddf9ca559e345834a885a49
Makoto Uwaya <makoto.uwaya@gmail.com>
2020-02-10 11:21:01 +0900

    refs #28 注文履歴を取得して画面に表示する

--- src/views/Archive.vue
+++ src/views/Archive.vue
@@ -1,10 +1,33 @@
 <template>
-  <v-col align="center">
-    <div class="about">
-      <h1>アーカイブ画面作成予定</h1>
-    </div>
-    <div class="mt-10">
+  <v-col cols="12" align="center">
+    <v-row>
+      <h1>注文入力履歴</h1>
+    </v-row>
+    <v-row>
+      <orderList :orders="orders.dataSet" />
+    </v-row>
+    <v-row class="mt-10">
       <v-btn x-large to="/">メニュー画面へ戻る</v-btn>
-    </div>
+    </v-row>
   </v-col>
 </template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import { productTypesMap, order } from "@/types/order";
+import { orderCollection } from "@/modules/orderModule";
+import OrderList from "@/components/OrderList.vue";
+
+@Component({
+  components: {
+    OrderList
+  }
+})
+export default class Archive extends Vue {
+  orders: orderCollection = new orderCollection();
+
+  async mounted() {
+    await this.orders.atouch();
+  }
+}
+</script>
